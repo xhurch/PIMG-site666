9a7736bdab2d62461e25e10a0d1864ad57e4d507
centralmeridian <mayoral2020@gmail.com>
2017-02-05 23:16:47 -0800

    putting the files in my branch

---
+++ site-header.php
@@ -0,0 +1,54 @@
+ <title>PIMG</title>
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+   
+    
+    <title>PIMG</title>
+    <meta name="description" content="The awe-striking power of virtual reality is evident to most anyone who tries it: a visceral feeling of being immersed in a fantasy, a full body epiphany one must experience first hand to believe.">
+    <meta name="keywords" content="virtual reality, portland immersive media group, pimg">
+    <?php 
+        $title = 'Portland Immersive Media Group';
+        $desc = 'The awe-striking power of virtual reality is evident to most anyone who tries it: a visceral feeling of being immersed in a fantasy, a full body epiphany one must experience first hand to believe.';
+
+    ?>
+
+    <meta name="abstract" content="<?=$desc ?>" />
+    <meta name="description" content="Portland Immersive Media Group" />
+    <meta name="ROBOTS" content="ALL"/>
+    <meta name="twitter:site" content="@pdximmersive" />
+    <meta name="twitter:title" content="<?=$title ?>">
+   
+    
+    <META HTTP-EQUIV="Pragma" CONTENT="no-cache" /> 
+    <META HTTP-EQUIV="Expires" CONTENT="-1" />
+    <meta name="copyright" content="" />
+    <link rel="image_src" href="tn.jpg" /> 
+
+    <meta itemprop="name" content="<?=$title ?>"/>
+    <meta itemprop="description" content="<?=$desc ?>/>
+    <meta itemprop="image" content="tn.jpg"></meta>
+
+
+    <meta property="og:image" content="tn.jpg" />
+    <meta property="og:image:width" content="1024" />
+    <meta property="og:image:height" content="512" />
+
+    <meta property="og:title" content="<?=$title ?>" />
+    <meta property="og:url" content="vrspa.club" />
+    <meta property="og:description" content="<?=$title ?>" />
+    <meta name="twitter:card" content="product"></meta>
+    <meta name="twitter:site" content="@pdximmersive"></meta>
+    <meta name="twitter:creator" content="@pdximmersive"></meta>
+    <meta name="twitter:domain" content="vrspa.club"></meta><meta name="twitter:title" content="<?= $title ?>"></meta>
+    <meta name="twitter:description" content=" <?= $title ?>"></meta>
+    <meta name="twitter:image" content="tn.jpg"></meta>
+   
+
+
+
+	<script src="https://aframe.io/releases/0.4.0/aframe.min.js"></script>
+    <script src="js/aframe-animation-component.js"></script>
+    <script src="js/aframe-hyperlink.js"></script>
+    <script src="js/sw-load.js" data-service-worker="../sw.js" data-service-worker-scope="../"></script>
+	<link rel="stylesheet" type="text/css" href="PIMG.css">
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
