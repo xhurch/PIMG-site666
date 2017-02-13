69d8603b66051b1dbbcb45e2d05a8ae57b45c45c
centralmeridian <mayoral2020@gmail.com>
2017-02-12 17:54:51 -0800

    update

--- test-scott.php
+++ test-scott.php
@@ -25,38 +25,7 @@
         <div class="sidebar__wrap">
           <div class="sidebar__wrap__inner">
 
-            <ul>
-              <!-- HEADER -->
-              <div class="w3-col m4 w3-center">
-                <li class="nav-list" id="home"><a class="main" href="index.shtml" >Portland Immersive Media Group</a></li>
-              </div>
-              <!-- --- -->
-
-              <!-- NAV -->  
-              <div class="w3-col m1 w3-center">
-                <li class="nav-list" id="about"><button data-id="aboot" class="menu">About</button></li>
-              </div>  
-              <div class="w3-col m1 w3-center">
-                <li class="nav-list" id="members"><button data-id="group" class="menu">Us</button></li>
-              </div>  
-              <div class="w3-col m2 w3-center">  
-                <li class="nav-list" id="past"><button data-id="ea" class="menu">Exhibitions & Awards</button></li>
-              </div>
-              <div class="w3-col m1 w3-center">  
-                <li class="nav-list" id="contacts"><button data-id="contact" class="menu">Contact</button></li>
-              </div>
-              <!-- --- -->
-
-              <!-- ICONS -->
-              <div class="w3-col m3 w3-center">  
-                <li class="nav-list" id="steam"><a href="http://store.steampowered.com/steamvr" ><img src="images/steam.png" id="social"></a></li>
-                <li class="nav-list" id="twitter"><a href="https://twitter.com/_pimg"><img src="images/twitter.png" id="social"></a></li>
-                <li class="nav-list" id="instagram"><a href="https://www.instagram.com/pdximmersivemediagroup/"><img src="images/instagram.png" id="social"></a></li>
-                <li class="nav-list" id="vimeo"><a href="https://vimeo.com/user47660576" ><img src="images/vimeo.png" id="social"></a></li>
-              </div> 
-              <!-- --- --> 
-            </ul>
-
+            <?php include_once('header.php'); ?>
             <div data-id="none" class="cancel"></div>
 
           </div>
